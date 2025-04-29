dd7feed6d820b7fad79a6dd6170c593cf4c946d7
WenLi <90953539+liw95@users.noreply.github.com>
2025-04-29 23:40:40 +0800

    Update lidarloc.py
    Fixed bugs in dataloader

--- datasets/lidarloc.py
+++ datasets/lidarloc.py
@@ -202,7 +202,10 @@ class LiDARLocDataset(Dataset):
             num = 0
             for i in idx:
                 scan_path = self.pcs[i]
-                ptcld = np.fromfile(scan_path, dtype=np.float32).reshape(4, -1).transpose()[:, :3]
+                if self.scene == 'NCLT':
+                    ptcld = np.fromfile(scan_path, dtype=np.float32).reshape(-1, 4)[:, :3]
+                else:
+                    ptcld = np.fromfile(scan_path, dtype=np.float32).reshape(4, -1).transpose()[:, :3]
                 # just for z-axis up
                 ptcld[:, 2] = -1 * ptcld[:, 2]
 
@@ -244,7 +247,10 @@ class LiDARLocDataset(Dataset):
         else:
             # For Regression training
             scan_path = self.pcs[idx]
-            ptcld = np.fromfile(scan_path, dtype=np.float32).reshape(4, -1).transpose()[:, :3]
+            if self.scene == 'NCLT':
+                ptcld = np.fromfile(scan_path, dtype=np.float32).reshape(-1, 4)[:, :3]
+            else:
+                ptcld = np.fromfile(scan_path, dtype=np.float32).reshape(4, -1).transpose()[:, :3]
             # just for z-axis up
             ptcld[:, 2] = -1 * ptcld[:, 2]
             scan = ptcld
